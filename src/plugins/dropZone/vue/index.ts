cc1736266c36cf0ced1faac09a82c78ed3a63bcb
Sasha Milenkovic <sasha@formkit.com>
2024-01-31 12:35:55 -0500

    refactor: removes some artifact code

--- src/plugins/dropZone/vue/index.ts
+++ src/plugins/dropZone/vue/index.ts
@@ -1,19 +1,12 @@
-import type { DropZoneConfig } from "../types";
-
 import type { VueDropZones, VueDropZoneConfig } from "./types";
 
 import { handleVueElements } from "../../../vue/utils";
 
-import {
-  setupDropZone as setupDropZoneCore,
-  dropZones as dropZonePlugin,
-  dzData,
-} from "../index";
+import { setupDropZone as setupDropZoneCore } from "../index";
 
 export function dropZones(data: VueDropZones) {
   return (parent: HTMLElement) => {
     return {
-      tearDownParent() {},
       setupParent() {
         data.forEach((dz: VueDropZoneConfig) => {
           if (dz.el instanceof HTMLElement) {
@@ -26,40 +19,8 @@ export function dropZones(data: VueDropZones) {
         });
       },
     };
-    //const dzPlugin = dropZonePlugin()(parent);
-    //const dzPlugin = dropZonePlugin(dzConfig)(parent);
-    //if (!dzPlugin) return;
-    //return {
-    //  tearDownParent() {
-    //    dzPlugin.tearDownParent();
-    //  },
-    //  setupParent() {
-    //    dzConfig.dropZones?.forEach((dz: VueDropZone | VueElement) => {
-    //      console.log("drop zone");
-    //      if ("dropZone" in dz) {
-    //        handleVueElements(
-    //          dz.dropZone,
-    //          handleDropZone(parent, dz.config || {})
-    //        );
-    //      } else if (!("dropZone" in dz)) {
-    //        handleVueElements(dz, handleDropZone(parent, {}));
-    //      }
-    //    });
-    //    dzPlugin.setupParent();
-    //  },
-    //};
   };
 }
-
-//function setupDropzone(parent: HTMLElement, dzConfig: Partial<DropZoneConfig>) {
-//return (dropZone: HTMLElement) => {
-//  const dropZoneData = dzData.get(dropZone);
-
-//  if (!dropZoneData) return;
-
-//  setupDropZone(dropZone, dropZoneData);
-//};
-//}
 
 function setupDropZone(parent: HTMLElement, dzConfig: VueDropZoneConfig) {
   return (dropZone: HTMLElement) => {
