04f6615f5f87f38ba89b5370ff1836bba9480fff
Sasha Milenkovic <sasha@formkit.com>
2024-02-16 12:58:46 -0500

    feat: adds keydown handler for selections plugin

--- src/plugins/multiDrag/plugins/selections/index.ts
+++ src/plugins/multiDrag/plugins/selections/index.ts
@@ -7,7 +7,7 @@ import type {
 
 import type { SelectionsConfig } from "./types";
 
-import { parents, state, nodeEventData } from "../../../../index";
+import { parents, nodeEventData } from "../../../../index";
 
 import { addEvents, removeClass, addClass } from "../../../../utils";
 
@@ -25,6 +25,9 @@ export function selections(selectionsConfig: SelectionsConfig = {}) {
 
         parentData.config.handleClick =
           selectionsConfig.handleClick || handleClick;
+
+        parentData.config.handleKeydown =
+          selectionsConfig.handleKeydown || handleKeydown;
 
         selectionsConfig.clickawayDeselect =
           selectionsConfig.clickawayDeselect === undefined
@@ -53,8 +56,11 @@ export function selections(selectionsConfig: SelectionsConfig = {}) {
       setupNode(data: SetupNodeData) {
         const config = data.parentData.config;
 
+        data.node.setAttribute("tabindex", "0");
+
         const abortControllers = addEvents(data.node, {
           click: nodeEventData(config.handleClick),
+          keydown: nodeEventData(config.handleKeydown),
         });
 
         data.nodeData.abortControllers["selectionsNode"] = abortControllers;
@@ -102,22 +108,16 @@ export function selections(selectionsConfig: SelectionsConfig = {}) {
   //};
 }
 
-function handleRootClick(config: ParentConfig, e: Event) {
+function handleRootClick(config: ParentConfig) {
   for (const nodeRecord of multiDragState.selectedNodes) {
-    console.log("handle root click", nodeRecord);
     nodeRecord.el.classList.remove(config.selectionsConfig.selectedClass);
   }
 
   multiDragState.selectedNodes = [];
-  //const el = getElFromPoint({
-  //  e,
-  //  nodes: state.nodeData,
-  //  parents: state.parentData,
-  //});
-  //if (!el) return;
-  //if (el.node) {
-  //  handleClick(el);
-  //}
+}
+
+function handleKeydown(data: NodeEventData) {
+  keydown(data);
 }
 
 function handleClick(data: NodeEventData) {
@@ -142,8 +142,6 @@ function click(data: NodeEventData) {
   const ctParentData = data.targetData.parent.data;
 
   const selectedClass = selectionsConfig.selectedClass;
-
-  console.log("selectedClass", selectedClass);
 
   const targetNode = data.targetData.node;
 
@@ -242,24 +240,33 @@ function click(data: NodeEventData) {
       }
     }
   } else if (!commandKey) {
-    multiDragState.activeNode = {
-      el: data.targetData.node.el,
-      data: data.targetData.node.data,
-    };
-
-    if (selectedClass) {
-      for (const el of multiDragState.selectedNodes) {
-        removeClass([el.el], selectedClass);
+    if (multiDragState.selectedNodes.map((x) => x.el).includes(targetNode.el)) {
+      multiDragState.selectedNodes = multiDragState.selectedNodes.filter(
+        (el) => el.el !== targetNode.el
+      );
+      if (selectedClass) {
+        removeClass([targetNode.el], selectedClass);
       }
-
-      addClass([data.targetData.node.el], selectedClass);
-    }
-    multiDragState.selectedNodes = [
-      {
+    } else {
+      multiDragState.activeNode = {
         el: data.targetData.node.el,
         data: data.targetData.node.data,
-      },
-    ];
+      };
+
+      if (selectedClass) {
+        for (const el of multiDragState.selectedNodes) {
+          removeClass([el.el], selectedClass);
+        }
+
+        addClass([data.targetData.node.el], selectedClass);
+      }
+      multiDragState.selectedNodes = [
+        {
+          el: data.targetData.node.el,
+          data: data.targetData.node.data,
+        },
+      ];
+    }
   } else if (commandKey) {
     if (multiDragState.selectedNodes.map((x) => x.el).includes(targetNode.el)) {
       multiDragState.selectedNodes = multiDragState.selectedNodes.filter(
@@ -287,58 +294,53 @@ function click(data: NodeEventData) {
   }
 }
 
-function docClick(e: MouseEvent) {
-  for (const el of state.selectedNodes) {
-    const parentData = state.parentData.get(el.parentNode);
-    const multiDragConfigSelectedClass =
-      parentData?.multiDragConfig?.selectedClass;
-    if (multiDragConfigSelectedClass) {
-      el.classList.remove(multiDragConfigSelectedClass);
-    }
-  }
-  selectedNodes = [];
-  state.activeNode = undefined;
-}
+function keydown(data: NodeEventData) {
+  if (!(data.e instanceof KeyboardEvent)) return;
 
-function setEvents(el: Node) {
-  el.addEventListener("click", clickEvent);
-}
-
-function removeEvents(el: Node) {
-  el.removeEventListener("click", clickEvent);
-}
-
-function keydown(e: KeyboardEvent) {
   const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-  if (!keys.includes(e.key) || !state.activeNode) return;
-  e.preventDefault();
-  const parentData = state.parentData.get(state.activeNode.parentNode);
-  const nodeData = state.nodeData.get(state.activeNode);
-  if (!parentData || !nodeData) return;
+
+  if (!keys.includes(data.e.key) || !multiDragState.activeNode) return;
+
+  const selectionsConfig = data.targetData.parent.data.config.selectionsConfig;
+
+  data.e.preventDefault();
+
+  const parentData = data.targetData.parent.data;
+
+  const nodeData = data.targetData.node.data;
+
   const enabledNodes = parentData.enabledNodes;
-  if (!enabledNodes) return;
+
   const invalidKeydown =
-    ((e.key === "ArrowUp" || e.key === "ArrowRight") && nodeData.index === 0) ||
-    ((e.key === "ArrowDown" || e.key === "ArrowLeft") &&
+    ((data.e.key === "ArrowUp" || data.e.key === "ArrowRight") &&
+      nodeData.index === 0) ||
+    ((data.e.key === "ArrowDown" || data.e.key === "ArrowLeft") &&
       nodeData.index === enabledNodes.length - 1);
+
   if (invalidKeydown) return;
-  const moveUp = e.key === "ArrowUp" || e.key === "ArrowLeft";
+
+  const moveUp = data.e.key === "ArrowUp" || data.e.key === "ArrowLeft";
+
   const adjacentNode = enabledNodes[nodeData.index + (moveUp ? -1 : 1)];
-  const selectedClass = parentData?.multiDragConfig?.selectedClass;
+
+  const selectedClass = selectionsConfig.selectedClass;
+
   if (!adjacentNode) return;
-  if (e.altKey) {
-    if (state.selectedNodes.length > 1) {
-      for (const el of state.selectedNodes) {
-        if (selectedClass && state.activeNode !== el)
-          el.classList.remove(selectedClass);
+
+  if (data.e.altKey) {
+    if (multiDragState.selectedNodes.length > 1) {
+      for (const el of multiDragState.selectedNodes) {
+        if (selectedClass && multiDragState.activeNode !== el) {
+          removeClass([el.el], selectedClass);
+        }
       }
-      state.selectedNodes = state.selectedNodes.filter(
-        (el) => el !== state.activeNode
+
+      multiDragState.selectedNodes = multiDragState.selectedNodes.filter(
+        (el) => el !== multiDragState.activeNode
       );
     }
-    const parentData = state.parentData.get(adjacentNode.parentNode);
-    if (!parentData) return;
-    const parentValues = [...parentData.getValues(state.activeNode.parentNode)];
+    const parentValues = [parentData.getValues(data.targetData.parent.el)];
+
     [
       parentValues[nodeData.index],
       parentValues[nodeData.index + (moveUp ? -1 : 1)],
@@ -346,139 +348,55 @@ function keydown(e: KeyboardEvent) {
       parentValues[nodeData.index + (moveUp ? -1 : 1)],
       parentValues[nodeData.index],
     ];
-    parentData.setValues(state.activeNode.parentNode, parentValues);
-  } else if (e.shiftKey) {
-    if (!state.selectedNodes.includes(adjacentNode)) {
-      state.selectedNodes.push(adjacentNode);
-      if (selectedClass) adjacentNode.classList.add(selectedClass);
-      state.activeNode = adjacentNode;
+
+    parentData.setValues(parentValues, data.targetData.parent.el);
+  } else if (data.e.shiftKey) {
+    if (
+      !multiDragState.selectedNodes.map((x) => x.el).includes(adjacentNode.el)
+    ) {
+      multiDragState.selectedNodes.push(adjacentNode);
+
+      if (selectedClass) {
+        addClass([adjacentNode.el], selectedClass);
+      }
+
+      multiDragState.activeNode = adjacentNode;
     } else {
-      if (state.selectedNodes.includes(state.activeNode)) {
-        state.selectedNodes = state.selectedNodes.filter(
-          (el) => el !== state.activeNode
+      if (
+        multiDragState.selectedNodes
+          .map((x) => x.el)
+          .includes(multiDragState.activeNode.el)
+      ) {
+        multiDragState.selectedNodes = multiDragState.selectedNodes.filter(
+          (el) => el !== multiDragState.activeNode
         );
-        if (selectedClass) state.activeNode.classList.remove(selectedClass);
-        state.activeNode = adjacentNode;
+
+        if (selectedClass) {
+          removeClass([multiDragState.activeNode.el], selectedClass);
+        }
+
+        multiDragState.activeNode = adjacentNode;
       }
     }
   } else {
-    for (let el of state.selectedNodes) {
-      if (selectedClass && state.activeNode !== el)
-        el.classList.remove(selectedClass);
+    for (const el of multiDragState.selectedNodes) {
+      if (selectedClass && multiDragState.activeNode !== el) {
+        removeClass([el.el], selectedClass);
+      }
     }
-    state.activeNode.classList.remove(
-      parentData?.multiDragConfig?.selectedClass || ""
-    );
-    state.selectedNodes = [adjacentNode];
-    adjacentNode.classList.add(
-      parentData?.multiDragConfig?.selectedClass || ""
-    );
-    state.activeNode = adjacentNode;
-  }
-  state.activeNode.blur();
-}
 
-function clickEvent(e: MouseEvent) {
-  e.stopPropagation();
-  if (!isNode(e.currentTarget, state)) return;
-  const multiDragConfig = state.parentData.get(
-    e.currentTarget.parentNode
-  )?.multiDragConfig;
-  if (!multiDragConfig) return;
-  const commandKey = e.ctrlKey || e.metaKey;
-  const ctParentData = state.parentData.get(e.currentTarget.parentNode);
-  const selectedClass = ctParentData?.multiDragConfig?.selectedClass;
-  if (!ctParentData || !ctParentData.multiDragConfig) return;
-  if (e.shiftKey) {
-    if (!state.activeNode) {
-      state.activeNode = e.currentTarget;
-      const activeNodeData = state.nodeData.get(state.activeNode);
-      if (!activeNodeData) return;
-      for (let x = 0; x <= activeNodeData.index; x++) {
-        state.selectedNodes.push(ctParentData.enabledNodes[x]);
-        if (selectedClass)
-          ctParentData.enabledNodes[x].classList.add(selectedClass);
-      }
-    } else {
-      const activeNodeData = state.nodeData.get(state.activeNode);
-      const clickedNodeData = state.nodeData.get(e.currentTarget);
-      if (!activeNodeData || !clickedNodeData) return;
-      const [minIndex, maxIndex] =
-        activeNodeData.index < clickedNodeData.index
-          ? [activeNodeData.index, clickedNodeData.index]
-          : [clickedNodeData.index, activeNodeData.index];
-      const selectedNodes = ctParentData.enabledNodes.slice(
-        minIndex,
-        maxIndex + 1
-      );
-      if (selectedNodes.length === 1) {
-        for (const node of state.selectedNodes) {
-          if (selectedClass) node.classList.remove(selectedClass);
-        }
-        state.selectedNodes = [e.currentTarget];
-        state.activeNode = e.currentTarget;
-        if (selectedClass) e.currentTarget.classList.add(selectedClass);
-      }
-      for (let x = minIndex - 1; x >= 0; x--) {
-        if (state.selectedNodes.includes(ctParentData.enabledNodes[x])) {
-          state.selectedNodes = [
-            ...state.selectedNodes.filter(
-              (el) => el !== ctParentData.enabledNodes[x]
-            ),
-          ];
-          if (selectedClass)
-            ctParentData.enabledNodes[x].classList.remove(selectedClass);
-        } else {
-          break;
-        }
-      }
-      for (let x = maxIndex; x < ctParentData.enabledNodes.length; x++) {
-        if (state.selectedNodes.includes(ctParentData.enabledNodes[x])) {
-          state.selectedNodes = [
-            ...state.selectedNodes.filter(
-              (el) => el !== ctParentData.enabledNodes[x]
-            ),
-          ];
-          if (selectedClass)
-            ctParentData.enabledNodes[x].classList.remove(selectedClass);
-        } else {
-          break;
-        }
-      }
-      for (const node of selectedNodes) {
-        if (!state.selectedNodes.includes(node)) state.selectedNodes.push(node);
-        if (selectedClass) node.classList.add(selectedClass);
-      }
-    }
-  } else if (!commandKey) {
-    state.activeNode = e.currentTarget;
-    if (selectedClass) {
-      for (const el of state.selectedNodes) {
-        el.classList.remove(selectedClass);
-      }
-      state.activeNode.classList.add(selectedClass);
-    }
-    state.selectedNodes = [e.currentTarget];
-  } else if (commandKey) {
-    if (state.selectedNodes.includes(e.currentTarget)) {
-      state.selectedNodes = state.selectedNodes.filter(
-        (el) => el !== e.currentTarget
-      );
-      if (selectedClass) e.currentTarget.classList.remove(selectedClass);
-    } else {
-      state.activeNode = e.currentTarget;
-      if (selectedClass) e.currentTarget.classList.add(selectedClass);
-      state.selectedNodes.push(e.currentTarget);
-    }
+    removeClass([multiDragState.activeNode.el], selectedClass);
+
+    multiDragState.selectedNodes = [adjacentNode];
+
+    addClass([adjacentNode.el], selectedClass);
+
+    multiDragState.activeNode = adjacentNode;
   }
-  if (multiDragConfig.selected) {
-    const nodeData = state.nodeData.get(e.currentTarget);
-    if (!nodeData) return;
-    multiDragConfig.selected({
-      el: e.currentTarget,
-      nodeData,
-      parent: e.currentTarget.parentNode as HTMLElement,
-      parentData: ctParentData,
-    });
-  }
+
+  data.targetData.node.el.blur();
+
+  multiDragState.activeNode = adjacentNode;
+
+  multiDragState.activeNode.el.focus();
 }
--- src/plugins/multiDrag/plugins/selections/types.ts
+++ src/plugins/multiDrag/plugins/selections/types.ts
@@ -2,6 +2,7 @@ import type { NodeEventData } from "../../../../types";
 
 export interface SelectionsConfig {
   handleClick?: (data: NodeEventData) => void;
+  handleKeydown?: (data: NodeEventData) => void;
   selectedClass?: string;
   clickawayDeselect?: boolean;
 }
