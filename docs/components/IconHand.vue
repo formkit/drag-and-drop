3394a71ab2d8a6998e5c1838db4a27acc9fb4571
Andrew Boyd <andrew@formkit.com>
2024-02-16 17:10:55 -0500

    docs: header intro animation

---
+++ docs/components/IconHand.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  open: Boolean,
+  default: false,
+});
+
+// preload images
+onMounted(() => {
+  const img = new Image();
+  img.src = "/img/hand-open.png";
+  img.src = "/img/hand-closed.png";
+});
+</script>
+
+<template>
+  <img v-if="open" src="/img/hand-open.png" />
+  <img v-else src="/img/hand-closed.png" />
+</template>
--- docs/components/TheHero.vue
+++ docs/components/TheHero.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { dragAndDrop } from "@formkit/drag-and-drop/vue";
-import { animations } from "@formkit/drag-and-drop";
 
 const dragList = ref(null);
+const showHand = ref(true);
+const showTitle = ref(false);
+const openHand = ref(false);
+const exitHand = ref(false);
+const showHeadline = ref(false);
+const showDemo = ref(false);
 
 const features = ref([
   {
@@ -35,31 +40,116 @@ dragAndDrop({
   parent: dragList,
   values: features,
   draggingClass: "[&>.card]:-rotate-2 before:-rotate-2",
-  dropZoneClass: "blur-[2px] opacity-50",
-  plugins: [
-    animations({
-      duration: 200,
-    }),
-  ],
+  dropZoneClass: "blur-sm opacity-60",
+});
+
+onMounted(() => {
+  setTimeout(() => {
+    setTimeout(() => {
+      showTitle.value = true;
+    }, 0);
+    setTimeout(() => {
+      openHand.value = true;
+    }, 1000);
+    setTimeout(() => {
+      exitHand.value = true;
+    }, 1500);
+    setTimeout(() => {
+      showHand.value = false;
+    }, 2500);
+    setTimeout(() => {
+      showHeadline.value = true;
+    }, 1500);
+    setTimeout(() => {
+      showDemo.value = true;
+    }, 1700);
+  }, 200);
 });
 </script>
 
 <template>
   <div class="p-8 mt-40 flex flex-col items-center">
-    <h1 class="font-display text-9xl text-emerald-500 mb-5">
-      Drag<span class="text-emerald-800">&</span>Drop
-    </h1>
-    <p
-      class="text-4xl font-semibold text-center text-emerald-900 max-w-[500px] text-balance"
+    <h1
+      :data-show="showTitle"
+      :class="`
+      relative
+      font-display 
+      text-[10em] 
+      text-emerald-500 
+      mb-24
+      transition-all
+      duration-1000
+      -translate-x-[100vw]
+      data-[show=true]:translate-x-0
+    `"
     >
-      A tiny <span class="text-pink-600">declarative</span> library for the
-      modern web.
+      Drag<span class="text-emerald-800">&</span>Drop
+
+      <IconHand
+        v-if="showHand"
+        :data-exit="exitHand"
+        :class="`
+          absolute
+          -top-10
+          -right-10
+          -rotate-90
+          w-48
+          transition-all
+          duration-500
+          translate-x-0
+          data-[exit=true]:translate-x-[100vw]
+        `"
+        :open="openHand"
+      />
+    </h1>
+
+    <p
+      :data-show="showHeadline"
+      :class="`
+        text-6xl
+        font-semibold
+        text-center
+        text-emerald-900
+        max-w-[800px]
+        text-balance
+
+        transition-all
+        duration-500
+        translate-y-10
+        opacity-0
+        data-[show=true]:translate-y-0
+        data-[show=true]:opacity-100
+      `"
+    >
+      A <span class="text-3xl mr-2.5 text-green-600">tiny</span>
+      <span class="text-pink-600">declarative</span> library for the modern web.
     </p>
   </div>
 
   <ul
     ref="dragList"
-    class="features flex flex-wrap items-center justify-center mt-16 bg-emerald-400/10 border border-emerald-500 border-dashed rounded-xl p-5"
+    :data-show="showDemo"
+    :class="`
+      features
+      flex
+      flex-wrap
+      items-center
+      justify-center
+      mt-16
+      bg-emerald-100/40
+      border
+      border-emerald-500
+      border-dashed
+      rounded-xl
+      p-5
+
+      transition-all
+      duration-500
+      translate-y-10
+      opacity-0
+      data-[show=true]:translate-y-0
+      data-[show=true]:opacity-100
+    `"
   >
     <li
       v-for="feature in features"
@@ -69,6 +159,7 @@ dragAndDrop({
         z-10
         flex
         flex-col
+        basis-[30%]
         items-center
         m-2
         cursor-grab
@@ -83,18 +174,20 @@ dragAndDrop({
         before:-left-[3px]
         before:w-full
         before:h-full
+        before:rounded-md
       `"
     >
       <div
         :class="`
         card
-    
+        rounded-md
         p-5
         w-full
         bg-white
         border-t
         border-r
         border-emerald-500
+        text-center
       `"
       >
         <h2 class="text-xl font-semibold text-emerald-700 mb-1">
