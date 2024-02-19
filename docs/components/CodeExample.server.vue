ea88f7f201af2a680583248e73d29e0a97e6fae7
Justin Schroeder <justin@formkit.com>
2024-02-19 16:34:19 -0500

    wip: shiki

---
+++ docs/components/CodeExample.server.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { codeToHtml } from "shiki";
+import { transformerTwoslash } from "@shikijs/twoslash";
+
+const html = await codeToHtml('console.log("hello world")', {
+  theme: "solarized-light",
+  lang: "typescript",
+  transformers: [transformerTwoslash()],
+});
+</script>
+
+<template>
+  <div v-html="html"></div>
+</template>
