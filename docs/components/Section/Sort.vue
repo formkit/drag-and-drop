674973e342f858daff777062856e9b9577bbd806
Sasha Milenkovic <sasha@formkit.com>
2024-02-20 13:56:40 -0500

    docs: adds some docs content

---
+++ docs/components/Section/Sort.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2 class="text-xl">Sort</h2>
+  <p class="text-lg">
+    Without any further configuration, Drag and Drop will allow you to sort an
+    array of values. This is useful for reordering items in a list or for
+    creating a ranked list.
+  </p>
+  <Cassettes />
+</template>
