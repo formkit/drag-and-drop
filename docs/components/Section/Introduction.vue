1b1c3f41618e4958d7a58fbe65a1e93d7f2098a6
Sasha Milenkovic <sasha@formkit.com>
2024-02-20 10:42:45 -0500

    docs: edit to intro

--- docs/components/Section/Introduction.vue
+++ docs/components/Section/Introduction.vue
@@ -7,11 +7,12 @@
     for FormKitPro's taglist input. -->
     <!--<a href="https://formkit.com"></a>-->
     <a href="https://formkit.com/">FormKit's</a> Drag and Drop is a simple and
-    easy to use JavaScript library for enabling end-users to reorder DOM nodes
-    or to move DOM nodes between two separate elements. Unlike most comparable
-    packages, Drag and Drop does not perform any direct DOM manipulations to do
-    so. Instead, it takes a declarative approach by updating state and allowing
-    the reactive framework (of your choosing) to handle the rest.
+    easy to use JavaScript library for enabling end-users to sort an array of
+    values or to transfer values from one array to another. Unlike most
+    comparable packages, Drag and Drop does not perform any direct DOM
+    manipulations to do so. Instead, it takes a declarative approach by updating
+    state and allowing the reactive framework (of your choosing) to handle the
+    rest.
     <CodeExample example="example" />
     <div class="divider"></div>
     <h2>Getting started</h2>
