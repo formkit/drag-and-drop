562b1a4af915f7f89baab8e92fc4488c905630db
Andrew Boyd <andrew@formkit.com>
2024-02-21 13:31:49 -0500

    docs: functional sidebar

---
+++ docs/components/TheSidebar.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const h2s = ref<HTMLElement[]>([]);
+const h3s = ref<HTMLElement[]>([]);
+const headingsTree = new Map<HTMLElement, HTMLElement[]>();
+
+onMounted(() => {
+  h2s.value =
+    (Array.from(
+      document.querySelectorAll(".docs-content > h2")
+    ) as HTMLElement[]) || [];
+  h3s.value =
+    (Array.from(
+      document.querySelectorAll(".docs-content > h3")
+    ) as HTMLElement[]) || [];
+
+  // add ids to all headings based on their text content
+  const headings = Array.from(document.querySelectorAll("h2, h3"));
+  headings.forEach((heading) => {
+    const id = heading.textContent?.toLowerCase().replace(/\s/g, "-");
+    if (id) {
+      heading.id = id;
+    }
+  });
+
+  // create a tree of h2s and h3s
+  h2s.value.forEach((h2, i) => {
+    if (!h2s.value) return;
+    const nextH2 = h2s.value[i + 1];
+    const h3sUnderH2 = h3s.value.filter((h3) => {
+      if (nextH2) {
+        return h3.offsetTop > h2.offsetTop && h3.offsetTop < nextH2.offsetTop;
+      }
+      return h3.offsetTop > h2.offsetTop;
+    });
+    headingsTree.set(h2, h3sUnderH2);
+  });
+
+  const hash = window.location.hash.slice(1);
+  const el = document.getElementById(hash);
+  if (el) {
+    // TODO: figure out why this delay is necessary
+    setTimeout(() => {
+      el.scrollIntoView({ behavior: "smooth", block: "start" });
+    }, 800);
+  }
+});
+</script>
+
+<template>
+  <aside
+    :class="`
+      docs-sidebar
+      hidden
+      lg:flex
+      flex-col
+      border-r
+      border-r-slate-400/50
+      min-[1400px]:ml-[5%]
+      min-[1600px]:ml-auto
+      dark:border-r-slate-700
+      w-full
+      max-w-[180px]
+      min-[1400px]:max-w-[220px]
+    `"
+  >
+    <nav
+      :class="`
+        docs-sidebar-nav
+        sticky
+        min-[1400px]:pr-10
+        px-2
+        pt-0
+        top-2
+        max-h-[calc(100dvh-0.5rem)]
+        overflow-auto
+      `"
+    >
+      <ul class="pb-5 pl-2">
+        <li v-for="section in h2s">
+          <a
+            :href="`#${section.id}`"
+            class="inline-block text-base text-cyan-800 hover:text-cyan-600 py-1 dark:text-fuchsia-300 dark:hover:text-fuchsia-100"
+          >
+            {{ section.textContent }}
+          </a>
+
+          <ul
+            v-if="headingsTree.get(section)"
+            class="relative ml-5 before:w-px before:bg-slate-400/50 before:absolute before:top-2 before:bottom-2 before:-left-5 dark:before:bg-fuchsia-300/25"
+          >
+            <li v-for="subSection in headingsTree.get(section)">
+              <a
+                :href="`#${subSection.id}`"
+                class="inline-block text-sm text-cyan-800/75 hover:text-cyan-600 py-1 dark:text-fuchsia-300/50 dark:hover:text-fuchsia-300"
+              >
+                {{ subSection.textContent }}
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -3,58 +3,59 @@
 <template>
   <TheHero />
   <main
-    class="relative z-20 pt-20 min-h-[200vh] border-t border-t-slate-100 shadow-[0_-2em_7em_2em_rgba(0,0,0,.33),0_-0.5em_1em_0.15em_rgba(0,0,0,0.25)] dark:border-t-slate-600 dark:shadow-[0_-2em_6em_1em_rgba(0,0,0,1)]"
+    class="page-container flex justify-center relative z-20 pt-20 min-h-[200vh] border-t border-t-slate-100 shadow-[0_-2em_7em_2em_rgba(0,0,0,.33),0_-0.5em_1em_0.15em_rgba(0,0,0,0.25)] dark:border-t-slate-600 dark:shadow-[0_-2em_6em_1em_rgba(0,0,0,1)]"
   >
-    <PageSection class="text-slate-700 dark:text-slate-200">
-      <div class="docs-content">
-        <h2>Introduction</h2>
-        <SectionIntroduction />
-        <h2>Core Features:</h2>
-        <h3>Sortability</h3>
-        <p>
-          Sortable lists are a common use case for drag and drop libraries. This
-          library makes it easy to create sortable lists with minimal
-          configuration.
-        </p>
-        <CodeExample example="sorting" />
-        <h3>Transferability</h3>
-        <p>
-          If the intention is to transfer values between lists, simply set the
-          `group` property in the configuration to the same value for each list.
-        </p>
-        <CodeExample example="transfer" />
-        <h3>Drag Handles</h3>
-        <p>
-          Drag handles are a common way to allow users to drag items without
-          accidentally dragging them when they don't intend to. To implement
-          drag handles, set the `dragHandle` property to a selector that will be
-          used to find the handle.
-        </p>
-        <CodeExample example="drag-handles" />
-        <h2>Configuration</h2>
-        <CodeExampleNative example="config" />
+    <TheSidebar />
+    <div
+      class="docs-content min-w-0 px-4 w-full max-w-[1000px] lg:pl-8 min-[1400px]:pl-12"
+    >
+      <h2>Introduction</h2>
+      <SectionIntroduction />
+      <h2>Core Features</h2>
+      <h3>Sortability</h3>
+      <p>
+        Sortable lists are a common use case for drag and drop libraries. This
+        library makes it easy to create sortable lists with minimal
+        configuration.
+      </p>
+      <CodeExample example="sorting" />
+      <h3>Transferability</h3>
+      <p>
+        If the intention is to transfer values between lists, simply set the
+        `group` property in the configuration to the same value for each list.
+      </p>
+      <CodeExample example="transfer" />
+      <h3>Drag Handles</h3>
+      <p>
+        Drag handles are a common way to allow users to drag items without
+        accidentally dragging them when they don't intend to. To implement drag
+        handles, set the `dragHandle` property to a selector that will be used
+        to find the handle.
+      </p>
+      <CodeExample example="drag-handles" />
+      <h2>Configuration</h2>
+      <CodeExampleNative example="config" />
 
-        <h2>Plugins</h2>
-        <p>
-          Plugins are a powerful way to extend the functionality of the library.
-          They can be used to add new features, modify existing ones, or even
-          create entirely new experiences.
-        </p>
-        <h3>Multi Drag</h3>
-        <p>
-          The multi-drag plugin allows you to select multiple items and drag
-          them together. Try selecting files like you would on your machine.
-        </p>
-        <CodeExample example="multi-drag" />
-        <p>
-          Notice in the last example we are passing a separate plugin to the
-          multi-drag plugin. This is the selections plugin, which sets things
-          like click handlers. If you would like to write your own logic (or
-          have already written logic for selecting items), you can pass your own
-          selected values to the `selections` property.
-        </p>
-        <CodeExampleNative example="multi-drag-config" />
-      </div>
-    </PageSection>
+      <h2>Plugins</h2>
+      <p>
+        Plugins are a powerful way to extend the functionality of the library.
+        They can be used to add new features, modify existing ones, or even
+        create entirely new experiences.
+      </p>
+      <h3>Multi Drag</h3>
+      <p>
+        The multi-drag plugin allows you to select multiple items and drag them
+        together. Try selecting files like you would on your machine.
+      </p>
+      <CodeExample example="multi-drag" />
+      <p>
+        Notice in the last example we are passing a separate plugin to the
+        multi-drag plugin. This is the selections plugin, which sets things like
+        click handlers. If you would like to write your own logic (or have
+        already written logic for selecting items), you can pass your own
+        selected values to the `selections` property.
+      </p>
+      <CodeExampleNative example="multi-drag-config" />
+    </div>
   </main>
 </template>
