5f7a5e60b1be127213b505f6ea7bfb918a37174b
Justin Schroeder <justin@formkit.com>
2024-02-22 11:40:14 -0500

    docs: imroves intro line

--- docs/components/Section/Introduction.vue
+++ docs/components/Section/Introduction.vue
@@ -3,9 +3,9 @@
     <h2>Introduction</h2>
     <p>
       <a href="https://formkit.com/">FormKit’s</a> Drag and Drop is a small
-      library for adding data-first drag and drop sorting and transferring
-      between lists to your app. It’s simple, flexible, framework agnostic, and
-      clocks in at only ~4Kb gzipped.
+      library for adding data-first drag and drop sorting and transferring for
+      lists in your app. It’s simple, flexible, framework agnostic, and clocks
+      in at only ~4Kb gzipped.
     </p>
   </SectionWrapper>
 </template>
