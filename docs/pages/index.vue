41f82cb9d3f136227b457c7d121c103adf0aa1fd
Sasha Milenkovic <sasha@formkit.com>
2024-02-22 08:53:05 -0500

    docs: updates copy

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -17,13 +17,13 @@
       <h2>Getting Started</h2>
       <p>
         Whether using a composable <code>useDragAndDrop</code> or a direct
-        invocation of the core <code>dragAndDrop</code>, the API remains largely
-        the same. The first parameter accepts an HTMLElement that represents the
-        "parent" or the element containing the draggable elements. The second
-        paramter is an array of values that are to be assigned to the draggable
-        elements. The third parameter is an optional configuration object. This
-        configuation object can be used to set characteristics/behaviors of the
-        given parent.
+        invocation of the core <code>dragAndDrop</code>, function the API
+        remains the same. The first parameter accepts an HTMLElement that
+        represents the "parent" or the element containing the draggable
+        elements. The second paramter is an array of values that are assigned to
+        the draggable elements. The third parameter is an optional configuration
+        object. This configuation object can be used to set
+        characteristics/behaviors of the given parent.
       </p>
       <h2>Core Features</h2>
       <h3>Sortability</h3>
