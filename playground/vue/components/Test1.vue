5d42161bdb974918ede71fdddf5c7e674310ffc1
Sasha Milenkovic <sasha@formkit.com>
2024-02-22 08:48:30 -0500

    fix: fixes issue where selected class was not applied to actively dragging node for multi-drag

--- playground/vue/components/Test1.vue
+++ playground/vue/components/Test1.vue
@@ -1,51 +1,83 @@
 <script setup lang="ts">
 import { useDragAndDrop } from "../../../src/vue/index";
-import { animations } from "../../../src/index";
+import { multiDrag, selections } from "../../../src/index";
 
-const props = defineProps<{
-  id: string;
-  testDescription: string;
-}>();
-const [parent, values] = useDragAndDrop(
-  [
-    {
-      id: "10_of_clubs",
-      src: "/cards/10_of_clubs.png",
-    },
-    {
-      id: "jack_of_hearts",
-      src: "/cards/jack_of_hearts.png",
-    },
-    {
-      id: "queen_of_spades",
-      src: "/cards/queen_of_spades.png",
-    },
+const mockFileNames = [
+  "dungeon_master.exe",
+  "map_1.dat",
+  "map_2.dat",
+  "character1.txt",
+  "character2.txt",
+  "shell32.dll",
+  "README.txt",
+];
+
+const [parent1, files1] = useDragAndDrop(mockFileNames, {
+  group: "A",
+  plugins: [
+    multiDrag({
+      plugins: [
+        selections({
+          selectedClass: "red",
+        }),
+      ],
+    }),
   ],
-  {
-    plugins: [animations()],
-    dropZoneClass: "blur-[2px] opacity-60",
-  }
-);
+});
+
+const [parent2, files2] = useDragAndDrop([], {
+  group: "A",
+  plugins: [
+    multiDrag({
+      plugins: [
+        selections({
+          selectedClass: "red",
+        }),
+      ],
+    }),
+  ],
+});
 </script>
 
 <template>
-  <div>
-    <h3>#{{ props.id }}</h3>
-    <h4>
-      {{ props.testDescription }}
-    </h4>
-    <ul ref="parent">
-      <li
-        v-for="card in values"
-        :key="card.id"
-        class="item"
-        :id="props.id + '_' + card.id"
-      >
-        <img :src="`${card.src}`" />
-      </li>
-    </ul>
-    <span :id="props.id + '_values'">
-      {{ values.map((x) => x.id).join(" ") }}
-    </span>
+  <div name="multiDrag">
+    <div
+      class="group bg-slate-500 dark:bg-slate-800 data-[handles=true]:bg-emerald-700 dark:data-[handles=true]:bg-emerald-950"
+    >
+      <div class="p-2 mr-2 flex justify-between font-oldschool">
+        <ul
+          ref="parent1"
+          class="bg-white border-4 border-slate-400 w-full p-3 dark:bg-neutral-700"
+        >
+          <li
+            v-for="item in files1"
+            :key="item"
+            class="py-2 px-4 last:mb-0 !text-2xl"
+          >
+            {{ item }}
+          </li>
+        </ul>
+
+        <ul
+          ref="parent2"
+          class="bg-white border-4 border-slate-400 ml-2 w-full p-3 dark:bg-neutral-700"
+        >
+          <li
+            v-if="files2.length"
+            v-for="item in files2"
+            :key="item"
+            class="py-2 px-4 last:mb-0 !text-2xl"
+          >
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.red {
+  background-color: red !important;
+}
+</style>
