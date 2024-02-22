d6fd342b7ed998bca5848df53c653bc09bd23528
Andrew Boyd <andrew@formkit.com>
2024-02-22 12:48:20 -0500

    docs: condenses accepts example

--- docs/examples/accepts/accepts.tsx
+++ docs/examples/accepts/accepts.tsx
@@ -2,17 +2,9 @@ import React from "react";
 import type { ParentConfig } from "@formkit/drag-and-drop";
 import { useDragAndDrop } from "@formkit/drag-and-drop/react";
 
-export function myComponent() {
+export default function myComponent() {
   const [source, items1] = useDragAndDrop<HTMLUListElement, string>(
-    [
-      "dungeon_master.exe",
-      "map_1.dat",
-      "map_2.dat",
-      "character1.txt",
-      "character2.txt",
-      "shell32.dll",
-      "README.txt",
-    ],
+    ["dungeon_master.exe", "map_1.dat", "map_2.dat", "character1.txt", "character2.txt", "shell32.dll", "README.txt",],
     {
       draggable: (el) => {
         return el.id !== "no-drag";
@@ -21,28 +13,23 @@ export function myComponent() {
   );
 
   const config1: Partial<ParentConfig<string>> = {};
-
   config1.accepts = (_parent, lastParent) => {
     if (lastParent.el === target2.current) {
       return false;
     }
-
     return items2.length < 3;
   };
 
   const config2: Partial<ParentConfig<string>> = {};
-
   config2.accepts = (_parent, lastParent) => {
     if (lastParent.el === target1.current) {
       return false;
     }
-
     return items3.length < 5;
   };
 
-  const [target1, items2] = useDragAndDrop(["item1", "item2"], config1);
-
-  const [target2, items3] = useDragAndDrop(["item3", "item4"], config2);
+  const [target1, items2] = useDragAndDrop(["knight.bmp", "dragon.bmp"], config1);
+  const [target2, items3] = useDragAndDrop(["brick.bmp", "moss.bmp"], config2);
 
   return (
     <div>
@@ -65,22 +52,3 @@ export function myComponent() {
     </div>
   );
 }
-
-export default myComponent;
-
-const [source, items1] = useDragAndDrop(
-  [
-    "dungeon_master.exe",
-    "map_1.dat",
-    "map_2.dat",
-    "character1.txt",
-    "character2.txt",
-    "shell32.dll",
-    "README.txt",
-  ],
-  {
-    accepts: () => {
-      return true;
-    },
-  }
-);
