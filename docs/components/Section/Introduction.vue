54af375a452c532626e3ad87c61874611ae1d849
Justin Schroeder <justin@formkit.com>
2024-02-22 11:37:28 -0500

    docs: adds basic usage section

--- docs/components/Section/Introduction.vue
+++ docs/components/Section/Introduction.vue
@@ -7,16 +7,5 @@
       between lists to your app. It’s simple, flexible, framework agnostic, and
       clocks in at only ~4Kb gzipped.
     </p>
-    <h3>Installation</h3>
-    <p>
-      Drag and Drop is published as <code>@formkit/drag-and-drop</code> on npm.
-      Click to copy the install command for your package manager of choice:
-    </p>
-    <ul class="my-8 flex flex-col md:grid grid-cols-2 gap-4 max-w-2xl">
-      <li><CopyCodeSingle command="npm install @formkit/drag-and-drop" /></li>
-      <li><CopyCodeSingle command="pnpm add @formkit/drag-and-drop" /></li>
-      <li><CopyCodeSingle command="yarn add @formkit/drag-and-drop" /></li>
-      <li><CopyCodeSingle command="bun install @formkit/drag-and-drop" /></li>
-    </ul>
   </SectionWrapper>
 </template>
