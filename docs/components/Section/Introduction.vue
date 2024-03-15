ae35dcc22a68ee690cd668608b0631634d9aee5d
Luan Nguyen <luan@wearebraid.com>
2024-03-15 12:40:37 -0400

    docs: Adds supported frameworks

--- docs/components/Section/Introduction.vue
+++ docs/components/Section/Introduction.vue
@@ -5,7 +5,8 @@
       <a href="https://formkit.com/?utm_source=dnd&utm_medium=website&utm_campaign=body">FormKit’s</a>
       Drag and Drop is a small library for adding data-first drag and drop
       sorting and transferring for lists in your app. It’s simple, flexible,
-      framework agnostic, and clocks in at only ~4Kb gzipped.
+      framework agnostic, and clocks in at only ~4Kb gzipped. Works with
+      React, Solid, Vue, Svelte, or any JavaScript application.
     </p>
   </SectionWrapper>
 </template>
