8772e3d78884f6b1e9004f883f303e9c03800c35
Sasha Milenkovic <sasha@formkit.com>
2024-09-30 01:35:40 -0400

    comments out majority of docs for now

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -13,12 +13,12 @@
       class="docs-content min-w-0 px-4 w-full max-w-[1000px] lg:pl-8 xl:pl-12"
     >
       <SectionIntroduction />
-      <SectionGettingStarted />
+      <!--<SectionGettingStarted />
       <SectionFeatures />
       <SectionPlugins />
       <SectionEvents />
       <SectionConfiguration />
-      <SectionSupport />
+      <SectionSupport />-->
     </div>
   </main>
   <TheFooter />
