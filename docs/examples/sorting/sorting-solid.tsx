59c906f95cee917c9d1d121a5687003f11240dfc
Sasha Milenkovic <sasha@formkit.com>
2025-02-19 08:31:54 -0500

    internal: fixes another import for solidjos in docs

--- docs/examples/sorting/sorting-solid.tsx
+++ docs/examples/sorting/sorting-solid.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource solid-js */
 import { For, type JSX } from "solid-js";
-import { useDragAndDrop } from "../../../src/solid/index";
+import { useDragAndDrop } from "@formkit/drag-and-drop/solid";
 
 export const MyComponent = (): JSX.Element => {
   const [parent, tapes] = useDragAndDrop<HTMLUListElement, string>([
